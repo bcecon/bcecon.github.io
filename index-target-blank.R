c628ae7f8f3598b43805902e9c9e451f13f9d778
bcecon <to-_-v@hanmail.net>
2025-08-24 23:05:56 -0400

    fall-2025

--- index-target-blank.R
+++ index-target-blank.R
@@ -8,37 +8,25 @@ html_as_text <- readLines("index.html", warn = FALSE)
 system(paste("rm", shQuote("index.html")))
 # Sys.sleep(2)
 
-# Next lecture number & dates 
-lec_dates <- c('0826', '0828', '0830', 
-               '0904', '0906', 
-               '0909', '0911', '0913', 
-               '0916', '0918', '0920', 
-               '0925', '0927',
-               '0930', '1002', '1004',
-               '1016', '1018', 
-               '1021', '1023', '1025',
-               '1028', '1030', 
-               '1104', '1106', '1108',
-               '1111', '1113', '1115',
-               '1125',
-               '1202')
-lec_n <- length(lec_dates) 
+# Next lecture number & dates
+lec_dates <- c('0825')
+lec_n <- length(lec_dates)
 
-lec_nos <- 1:lec_n 
+lec_nos <- 1:lec_n
 lec_nos <- sprintf("%02d", lec_nos)
 
 for (i in 1:length(lec_nos)){
   lec_no <- lec_nos[i]
   lec_date <- lec_dates[i]
   
-  old_content <- str_c('./econ-lec/econ-341-lec-', lec_no, '-2024-', lec_date, '.html')
+  old_content <- str_c('./econ-lec/econ-340-lec-', lec_no, '-2025-', lec_date, '.html')
   new_content <- str_c('#', 
                        '" onclick="', 
                        "window.open(", 
                        "'", 
                        old_content, 
                        "'); return false;", '"') 
-
+  
   html_as_text <- str_replace_all(html_as_text,
                                   old_content,
                                   new_content)
@@ -47,6 +35,3 @@ for (i in 1:length(lec_nos)){
 writeLines(html_as_text, 
            "index.html")
 
-# <a href="./econ-lec/econ-341-lec-01-2024-0826.html" class="title listing-title">Lecture 1</a>
-# <a href="#" onclick="window.open('./econ-lec/econ-341-lec-01-2024-0826.html'); return false;" class="title listing-title">Lecture 1</a>
-  
