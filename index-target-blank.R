6e61dd9128e8f3ca1c099d8f1138c99aaa543bdf
bcecon <to-_-v@hanmail.net>
2025-09-30 04:39:43 -0400

    cw4

--- index-target-blank.R
+++ index-target-blank.R
@@ -10,7 +10,7 @@ system(paste("rm", shQuote("index.html")))
 
 # Next lecture number & dates
 lec_dates <- c('0825', '0827', '0829', '0905',
-               '0915', '0926', '1001')
+               '0915', '0926', '1003')
 lec_n <- length(lec_dates)
 
 lec_nos <- 1:lec_n
